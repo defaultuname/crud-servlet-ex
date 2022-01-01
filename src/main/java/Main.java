9f04e5e6c80635a1f4cbde3591b269046a785d6f
defuname <westn666@gmail.com>
2022-01-01 11:17:26 +0300

    some fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import ch.qos.logback.classic.Logger;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.loader.WebappLoader;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
@@ -38,6 +39,8 @@ public class Main {
 
         StandardContext ctx = (StandardContext) tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
         logger.info("Configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+        WebappLoader loader = new WebappLoader(Thread.currentThread().getContextClassLoader());
+        ctx.setLoader(loader);
 
         File additionWebInfClasses = new File("target/classes");
         WebResourceRoot resources = new StandardRoot(ctx);
